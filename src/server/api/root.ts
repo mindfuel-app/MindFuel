54d9868bb3c9d1dbb353e4748b41b4043d32b0e9
IanPol0 <101563167+IanPol0@users.noreply.github.com>
2023-06-01 11:38:23 -0300

    fix: ignored examplerouter

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,4 @@
-import { exampleRouter } from "~/server/api/routers/example";
+//import { exampleRouter } from "~/server/api/routers/example";
 import { createTRPCRouter } from "~/server/api/trpc";
 
 /**
@@ -7,7 +7,7 @@ import { createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
+  //example: exampleRouter,
 });
 
 // export type definition of API
