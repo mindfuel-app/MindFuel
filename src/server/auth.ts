e933c72ab6dce43bf7d7e297f5360e64dd7c14d7
IanPol0 <101563167+IanPol0@users.noreply.github.com>
2023-06-01 11:19:29 -0300

    fix: sign in with google

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -55,6 +55,13 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: env.GOOGLE_CLIENT_ID,
       clientSecret: env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code"
+        }
+      }
     }),
     /**
      * ...add more providers here.
