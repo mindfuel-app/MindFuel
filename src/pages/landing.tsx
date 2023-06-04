52f545fc426c0106053914e908b816649fca6fd9
dangoldmann <goldmandan8@gmail.com>
2023-06-04 15:33:38 -0300

    feat: Created landing page

---
+++ src/pages/landing.tsx
@@ -0,0 +1,31 @@
+import Head from "next/head";
+import Link from "next/link";
+
+export default function Landing() {
+    return (
+        <>
+            <Head>
+                <title>Landing</title>
+            </Head>
+            <div className="flex flex-col py-2">
+                <div className="flex px-5 py-3 justify-end">
+                    <Link href="/login">
+                        <span className="text-lg">Login</span>
+                    </Link>
+                </div>
+                <div className="flex flex-col justify-between">
+                    <div className="flex flex-col items-center py-40 px-4 space-y-20">
+                        <h1 className="text-5xl font-bold">MindFuel</h1>
+                        <p className="text-lg">MindFuel es una solución diseñada para ayudar a las personas diagnosticadas con trastorno por déficit de atención e hiperactividad (TDAH) en la generación de hábitos sustentables en el tiempo y mantención de la atención. </p>
+                    </div>
+                    <div className="flex justify-center ">
+                        <Link href="/login" className="bg-white rounded-full py-3 px-28 mt-20 shadow-md">
+                            <span className="text-lg text-black font-bold ">EMPEZAR</span>
+                        </Link>
+                    </div>
+                </div>
+                
+            </div>
+        </>
+    )
+}
