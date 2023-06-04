5e573970c694272913734b7c23dbc0cf32495219
dangoldmann <goldmandan8@gmail.com>
2023-06-04 17:56:08 -0300

    Added getInitialProps to document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,12 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript, type DocumentContext } from 'next/document'
 
 class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+
   render() {
     return (
       <Html>
