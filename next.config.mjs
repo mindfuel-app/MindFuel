3ba53f43204d4eb4647b40a4c474387334004b48
dangoldmann <goldmandan8@gmail.com>
2023-06-05 20:19:02 -0300

    feat: Added PWA

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,25 @@
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
- */
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
 await import("./src/env.mjs");
 
-/** @type {import("next").NextConfig} */
-const config = {
-  reactStrictMode: true,
+import withPWA from 'next-pwa'
 
-  /**
-   * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
-   * out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
-};
-export default config;
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
+    register: true,
+    skipWaiting: true,
+}
+
+const buildConfig = _phase => {
+  const plugins = [withPWA]
+  const config = plugins.reduce((acc, next) => next(acc), {
+    ...nextConfig,
+  })
+  return config
+}
+
+export default buildConfig
