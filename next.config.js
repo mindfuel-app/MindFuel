c504ad78cd9b43f8ae398e257af70226cf7332ca
dangoldmann <goldmandan8@gmail.com>
2023-06-05 22:51:16 -0300

    feat: Updated PWA

---
+++ next.config.js
@@ -0,0 +1,14 @@
+const withPWA = require('next-pwa')({
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
+    register: true,
+    skipWaiting: true,
+})
+
+module.exports = withPWA({
+    reactStrictMode: true,
+    i18n: {
+        locales: ["en"],
+        defaultLocale: "en",
+    },
+})
