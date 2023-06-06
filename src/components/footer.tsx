a2f2b9b1998ccf85daef5809adfbad0adba632fc
mindfuel-app <135031499+mindfuel-app@users.noreply.github.com>
2023-06-06 10:37:05 -0300

    Added background change

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,6 +2,7 @@ import Link from "next/link"
 import {FaTasks, FaUserFriends} from "react-icons/fa"
 import {CiStar} from "react-icons/ci"
 import {RxCalendar} from "react-icons/rx"
+import { useState } from "react"
 
 export default function Footer() {
     const navigation = [
@@ -11,19 +12,24 @@ export default function Footer() {
         { name: 'Comunidad', href: '/comunidad', icon: <FaUserFriends className="text-3xl"/> },
     ]
 
+    const [activeElement, setActiveElement] = useState('Tareas')
+
     return (
         <>
             <footer className="w-full bg-white px-4 py-5 fixed bottom-0">
                 <div className="flex justify-around gap-7">
                     {navigation.map(item => {
                         return (
-                            <Link
-                                key={item.name}
-                                href={item.href}
-                                className=""
-                            >
-                                {item.icon ? item.icon : item.name}
-                            </Link>
+                            <div key={item.name} className={activeElement == item.name ? 'bg-slate-400  px-4 py-2' : 'px-4 py-2'}>
+                                <Link
+                                    href='/tareas'
+                                    className=""
+                                    onClick={() => setActiveElement(item.name)}
+                                >
+                                    {item.icon}
+                                </Link>
+                            </div>
+                            
                         )
                     })}
                 </div>
