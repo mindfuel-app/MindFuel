c937225dd72702d8719a84539b992b09dffcf7db
mindfuel-app <135031499+mindfuel-app@users.noreply.github.com>
2023-06-06 14:26:14 -0300

    probar error auth

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -51,14 +51,7 @@ export const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
       clientId: env.GOOGLE_CLIENT_ID,
-      clientSecret: env.GOOGLE_CLIENT_SECRET,
-      authorization: {
-        params: {
-          prompt: "consent",
-          access_type: "offline",
-          response_type: "code"
-        }
-      }
+      clientSecret: env.GOOGLE_CLIENT_SECRET
     }),
     CredentialsProvider({
       name: "credentials",
