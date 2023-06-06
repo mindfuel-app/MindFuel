6f268088589e7fc9c1f78c915c9905aa3c4100c1
mindfuel-app <135031499+mindfuel-app@users.noreply.github.com>
2023-06-06 10:36:23 -0300

    Created app layout

---
+++ src/components/layout.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import Footer from "./footer";
+import { signOut, useSession } from "next-auth/react";
+
+export default function AppLayout({ children }: { children: React.ReactNode }) {
+    const {data: sessionData} = useSession()
+
+    return (
+        <>
+            <div className="flex justify-between py-3 px-2 items-center">
+                <h1>{sessionData && <p>Bienvenido {sessionData.user.name}</p>}</h1>
+                {sessionData && <Link className="border-2 border-black rounded-xl px-2 py-1" href="" onClick={() => void signOut({callbackUrl: '/login'})}>Cerrar sesión</Link>}
+            </div>
+            {children}
+            <Footer/>
+        </>
+    )
+}
--- src/pages/tareas.tsx
+++ src/pages/tareas.tsx
@@ -1,18 +1,9 @@
-import { signOut, useSession } from "next-auth/react";
-import Link from "next/link";
-import Footer from "~/components/footer";
+import Layout from "~/components/layout";
 
 export default function ViewTasks() {
-    const {data: sessionData} = useSession();
-
     return (
-        <>
-            <div className="flex justify-between py-3 px-2 items-center">
-                <h1>{sessionData && <p>Bienvenido {sessionData.user.name}</p>}</h1>
-                {sessionData && <Link className="border-2 border-black rounded-xl px-2 py-1" href="" onClick={() => void signOut({callbackUrl: '/login'})}>Cerrar sesión</Link>}
-            </div>
-            <Footer/>
-        </>
-            
+        <Layout>
+            <div>Tareas</div>
+        </Layout>
     )
 }
