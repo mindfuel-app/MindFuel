2c11ad3cfafa1d6b20fa33321beb8624c3cbc290
mindfuel-app <135031499+mindfuel-app@users.noreply.github.com>
2023-06-08 10:35:48 -0300

    feat: Changed login to signIn

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -5,11 +5,11 @@ import Link from "next/link";
 import { CgClose } from "react-icons/cg";
 import { FcGoogle } from "react-icons/fc";
 
-export default function Login() {
+export default function SignIn() {
   return (
     <>
       <Head>
-        <title>Login</title>
+        <title>Sign in</title>
       </Head>
       <main className="flex min-h-screen flex-col py-10">
         <div className="flex justify-end pr-7 ">
@@ -20,7 +20,7 @@ export default function Login() {
         <div className="flex flex-col items-center py-28">
           <Image alt="Logo" src="/favicon.ico" width={75} height={58}></Image>
           <h1 className="mb-4 mt-10 text-2xl font-semibold">
-            Crear cuenta de MindFuel
+            Inicie sesión en MindFuel
           </h1>
           <div className="flex flex-col gap-5 rounded-2xl bg-white px-5 py-5 shadow-md">
             <Link
@@ -32,7 +32,6 @@ export default function Login() {
               <span>Sign up con Google</span>
             </Link>
           </div>
-          <div className="flex items-center"></div>
         </div>
       </main>
     </>
