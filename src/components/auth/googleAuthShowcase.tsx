1bab64ae01872bf1892e9969c46371b06cbd34ea
dangoldmann <goldmandan8@gmail.com>
2023-06-11 11:19:28 -0300

    feat: Created googleAuthShowcase component

---
+++ src/components/auth/googleAuthShowcase.tsx
@@ -0,0 +1,16 @@
+import { signIn } from "next-auth/react";
+import Link from "next/link";
+import { FcGoogle } from "react-icons/fc";
+
+export default function GoogleAuthShowcase() {
+  return (
+    <Link
+      href=""
+      className="no-highlight flex items-center justify-center space-x-2 rounded-2xl border-2 border-gray-200 px-8 py-2 text-lg active:bg-gray-100"
+      onClick={() => void signIn("google", { callbackUrl: "/tareas" })}
+    >
+      <FcGoogle className="text-2xl" />
+      <span className="text-">Sign in con Google</span>
+    </Link>
+  );
+}
--- src/components/auth/signInForm.tsx
+++ src/components/auth/signInForm.tsx
@@ -1,39 +1,32 @@
 import Link from "next/link";
-import { FcGoogle } from "react-icons/fc";
 import { signIn } from "next-auth/react";
 import Image from "next/image";
 import { Divider } from "@mui/material";
 import usePasswordToggle from "~/hooks/usePasswordToggle";
+import GoogleAuthShowcase from "./googleAuthShowcase";
 
 export default function SignInForm() {
   const [PasswordInputType, ToggleIcon] = usePasswordToggle();
 
   return (
-    <div className="flex flex-col items-center py-24">
+    <div className="flex flex-col items-center py-20">
       <Image alt="Logo" src="/favicon.ico" width={75} height={58}></Image>
       <h1 className="mb-4 mt-10 text-2xl font-semibold">
         Inicie sesión en MindFuel
       </h1>
       <div className="flex flex-col gap-2 rounded-2xl bg-white p-4 shadow-md">
-        <Link
-          href=""
-          className="flex items-center justify-center space-x-2 rounded-2xl border-2 border-gray-200 px-8 py-2 text-lg"
-          onClick={() => void signIn("google", { callbackUrl: "/tareas" })}
-        >
-          <FcGoogle className="text-2xl" />
-          <span className="text-">Sign in con Google</span>
-        </Link>
+        <GoogleAuthShowcase />
         <Divider variant="middle">o</Divider>
         <div className="flex flex-col justify-center space-y-5">
           <input
-            type="text"
+            type="email"
             placeholder="Email"
             className="w-full rounded-xl border-2 border-gray-300 px-3 py-1 focus:outline-sky-500"
           />
           <div className="input-group flex w-full items-center justify-between rounded-xl border-2 border-gray-300 px-3 py-1 focus-within:border-sky-500">
             <input
               type={PasswordInputType == "text" ? "text" : "password"}
-              placeholder="Password"
+              placeholder="Contraseña"
               className="outline-none"
             />
             <span className="text-gray-500">{ToggleIcon}</span>
@@ -41,7 +34,7 @@ export default function SignInForm() {
 
           <Link
             href=""
-            className="rounded-2xl bg-sky-500 px-5 py-2 text-center text-lg text-white"
+            className="no-highlight rounded-2xl bg-sky-500 px-5 py-2 text-center text-lg text-white active:bg-sky-300"
             onClick={() => void signIn("credentials")}
           >
             Sign in
