a302e0117f09a9b819bd7f95a9dcb2d0a37e0ffb
mindfuel-app <135031499+mindfuel-app@users.noreply.github.com>
2023-06-14 11:21:38 -0300

    feat: Changed button to link

--- src/components/auth/forgotPasswordButton.tsx
+++ src/components/auth/forgotPasswordButton.tsx
@@ -1,14 +1,12 @@
-import router from "next/router";
-import { Button } from "../ui/button";
+import Link from "next/link";
 
 export default function ForgotPasswordButton() {
   return (
-    <Button
-      variant="outline"
-      onClick={() => void router.push("/reestablecer-contraseña")}
-      className="no-highlight w-48 rounded-lg border-2 border-[#008080] bg-transparent font-medium text-[#008080] hover:bg-transparent active:bg-white/50 active:text-[#008080]"
+    <Link
+      href="/reestablecer-contraseña"
+      className="no-highlight font-medium underline-offset-[3px] active:underline"
     >
-      Olvidé la contraseña
-    </Button>
+      ¿Olvidaste tu contraseña?
+    </Link>
   );
 }
