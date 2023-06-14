d21e06359bd0dc0f443e0c2e5409476d8ea394c7
mindfuel-app <135031499+mindfuel-app@users.noreply.github.com>
2023-06-14 12:01:50 -0300

    feat: Applied animations to button component

--- src/components/auth/authButton.tsx
+++ src/components/auth/authButton.tsx
@@ -8,7 +8,7 @@ export default function AuthButton({
   return (
     <Button
       type="submit"
-      className="no-highlight w-full transform rounded-xl bg-[#008080] px-5 py-2 text-center text-lg text-white transition duration-200 ease-in-out active:scale-[.98] active:bg-[#008080]/70"
+      className="no-highlight w-full rounded-xl bg-[#008080] px-5 py-2 text-center text-lg text-white active:bg-[#008080]/70"
     >
       {method == "Sign up" ? "Registrarse" : "Inicia sesión"}
     </Button>
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -7,7 +7,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "~/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center rounded-md text-sm transition-colors focus-visible:outline-none disabled:opacity-50 disabled:pointer-events-none",
+  "inline-flex items-center justify-center rounded-md text-sm transition-colors focus-visible:outline-none disabled:opacity-50 disabled:pointer-events-none transform transition duration-200 ease-in-out active:scale-[.98]",
   {
     variants: {
       variant: {
