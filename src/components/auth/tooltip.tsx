1a56dc1b5c86046d8a5cb7752e535817135c62af
mindfuel-app <135031499+mindfuel-app@users.noreply.github.com>
2023-06-15 14:25:19 -0300

    feat: Working on tooltip

--- src/components/auth/tooltip.tsx
+++ src/components/auth/tooltip.tsx
@@ -1,11 +1,22 @@
 import { Tooltip } from "@mui/material";
 import { BsQuestionCircleFill } from "react-icons/bs";
-import { Button } from "../ui/button";
+import { useState } from "react";
 
 export default function HoverTooltip() {
+  const [showTooltip, setShowTooltip] = useState(false);
   return (
-    <Tooltip title={"Hola"}>
-      <div>Hola2</div>
-    </Tooltip>
+    <div>
+      <BsQuestionCircleFill
+        onClick={() => setShowTooltip(!showTooltip)}
+        className="relative"
+      />
+
+      {showTooltip && (
+        <div className="absolute z-10 mt-1 rounded bg-gray-700 p-2 font-normal text-white">
+          Tu nombre de usuario te identifica y es el que veran tus amigos en la
+          app
+        </div>
+      )}
+    </div>
   );
 }
