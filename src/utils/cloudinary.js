f8ad4e4d913106135da571936b5b8c95b8537aae
IanPol0 <ipolonuer@gmail.com>
2023-06-15 16:55:50 -0300

    started working on image uploading

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
@@ -10,7 +11,7 @@ export function uploadImage(imageUploaded) {
   return new Promise((resolve, reject) => {
     cloudinary.uploader.upload(
       imageUploaded,
-      { width: 400, height: 300, crop: "fill" },
+      { width: 400, height: 400, crop: "fill" },
       (err, res) => {
         if (err) reject(err);
         resolve(res);
