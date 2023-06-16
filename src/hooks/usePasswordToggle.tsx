71004ec80687327c1817e16fb4e73c03cc7d42b5
dangoldmann <goldmandan8@gmail.com>
2023-06-16 20:25:35 -0300

    fix: Made prop optional

--- src/hooks/usePasswordToggle.tsx
+++ src/hooks/usePasswordToggle.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
 
-export default function usePasswordToggle(isPasswordWrong: boolean) {
+export default function usePasswordToggle(isPasswordWrong?: boolean) {
   const [visible, setVisibility] = useState(false);
 
   const inputType = visible ? "text" : "password";
