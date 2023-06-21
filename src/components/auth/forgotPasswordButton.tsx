4dfc6bb05cda2b44f7ba21da1aa1599c4b790121
mindfuel-app <135031499+mindfuel-app@users.noreply.github.com>
2023-06-21 09:34:39 -0300

    fix: Fixed href

--- src/components/auth/forgotPasswordButton.tsx
+++ src/components/auth/forgotPasswordButton.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function ForgotPasswordButton() {
   return (
     <Link
-      href="/reestablecer-contraseña"
+      href="/reestablecer-contrasena"
       className="no-highlight font-medium underline-offset-[3px] active:underline"
     >
       ¿Olvidaste tu contraseña?
