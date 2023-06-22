7e045318355684977cec328a9edebd2933897f44
mindfuel-app <135031499+mindfuel-app@users.noreply.github.com>
2023-06-22 11:46:47 -0300

    fix: Fixed breakpoint por auth layout

--- src/components/auth/authPageLayout.tsx
+++ src/components/auth/authPageLayout.tsx
@@ -5,11 +5,11 @@ export default function authPageLayout({
 }) {
   return (
     <div className="flex bg-[#008080]">
-      <div className="h-screen w-0 xl:w-1/6"></div>
+      <div className="h-screen w-0 min-[1400px]:w-1/6"></div>
       <main className="z-100 flex min-h-screen w-full flex-col bg-[#edece7] py-10 xl:shadow-2xl">
         <div>{children}</div>
       </main>
-      <div className="h-screen w-0 xl:w-1/6"></div>
+      <div className="h-screen w-0 min-[1400px]:w-1/6"></div>
     </div>
   );
 }
