4f5b2cc660b8587b33280169c9a3f4f45bdaca56
dangoldmann <goldmandan8@gmail.com>
2023-06-23 20:40:44 -0300

    feat: Added responsiveness to 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,15 +3,17 @@ import Link from "next/link";
 export default function Custom404() {
   return (
     <div className="flex flex-col items-center gap-5 px-4 py-36">
-      <span className="mb-10 text-9xl text-gray-500">404</span>
-      <span className="text-3xl font-bold">Página no encontrada</span>
-      <p className="text-center text-lg">
+      <span className="mb-7 text-9xl text-gray-500">404</span>
+      <span className="text-3xl font-bold max-[360px]:text-2xl xl:text-4xl">
+        Página no encontrada
+      </span>
+      <p className="mt-3 text-center text-lg lg:text-xl">
         La página que intenta abrir no existe. Es posible que haya escrito mal
         la dirección o que la página se haya movido a otra URL.
       </p>
       <Link
         href="/tareas"
-        className="no-highlight mt-16 rounded-xl bg-[#5C7AFF] px-5 py-3 text-xl text-white active:bg-[#7992ff]"
+        className="no-highlight mt-10 rounded-xl bg-[#5C7AFF] px-5 py-3 text-xl text-white active:bg-[#7992ff]"
       >
         Volver a la home
       </Link>
