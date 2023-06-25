687668cd0d131c024f7bed2514f51b38e8e37cef
dangoldmann <goldmandan8@gmail.com>
2023-06-25 20:40:55 -0300

    feat: Created landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,15 @@
 import { useSession } from "next-auth/react";
 import Head from "next/head";
-import Link from "next/link";
 import Router from "next/router";
+import Logo from "~/components/auth/logo";
+import { Icon } from "@iconify/react";
+import { useState } from "react";
+import { Progress } from "../components/ui/progressBar";
 
 export default function Landing() {
   const { status } = useSession();
+  const [screenTouches, setScreenTouches] = useState(0);
+
   if (status === "authenticated") {
     void Router.push("/tareas");
   } else if (status === "unauthenticated") {
@@ -13,34 +18,75 @@ export default function Landing() {
         <Head>
           <title>MindFuel</title>
         </Head>
-        <div className="flex flex-col py-2">
-          <div className="flex justify-end px-5 py-3">
-            <Link
-              href="/signin"
-              className="no-highlight rounded-sm active:bg-gray-300"
-            >
-              <span className="text-lg">Sign in</span>
-            </Link>
-          </div>
-          <div className="flex flex-col justify-between">
-            <div className="flex flex-col items-center space-y-20 px-4 py-28">
-              <h1 className="text-5xl font-bold">MindFuel</h1>
-              <p className="text-lg">
-                MindFuel es una solución diseñada para ayudar a las personas
-                diagnosticadas con trastorno por déficit de atención e
-                hiperactividad (TDAH) en la generación de hábitos sustentables
-                en el tiempo y mantención de la atención.{" "}
+        <div
+          className="flex h-screen flex-col py-10"
+          onTouchStart={() => setScreenTouches(screenTouches + 1)}
+        >
+          <Logo />
+          {screenTouches == 0 && (
+            <div className="flex flex-col items-center gap-12 py-24 text-center font-semibold">
+              <h1 className="max-w-[300px] text-3xl text-[#008080] min-[360px]:text-4xl">
+                Bienvenido a MindFuel
+              </h1>
+              <p className="max-w-[280px] text-lg min-[360px]:text-xl">
+                Toque en cualquier parte de la pantalla para comenzar
               </p>
+              <Icon icon="carbon:touch-1" className="text-8xl" />
             </div>
-            <div className="flex justify-center ">
-              <Link
-                href="/signup"
-                className="no-highlight mt-12 rounded-full bg-white px-28 py-3 shadow-md active:bg-gray-100"
-              >
-                <span className="text-lg font-bold text-black ">EMPEZAR</span>
-              </Link>
+          )}
+          {screenTouches > 0 && (
+            <div className="flex flex-col px-6 py-3">
+              <h1 className="text-2xl font-semibold italic text-[#F57F11] sm:text-3xl">
+                Fuel your mind, fuel your life
+              </h1>
+              <div className="flex max-w-[500px] flex-col gap-7 py-8 text-left font-semibold sm:py-12 sm:text-lg">
+                {screenTouches > 1 && (
+                  <p>
+                    La{" "}
+                    <span className="font-extrabold">
+                      planificación de rutinas
+                    </span>{" "}
+                    es un paso importante que no puede faltar en nuestro día a
+                    día.
+                  </p>
+                )}
+                {screenTouches > 2 && (
+                  <p>
+                    Sin una{" "}
+                    <span className="font-extrabold">buena organización</span>{" "}
+                    existe la posibilidad de tener dificultades al completar
+                    tareas, concentrarse, mantener foco en una única cosa a la
+                    vez y vivir en el presente.
+                  </p>
+                )}
+                {screenTouches > 3 && (
+                  <p>
+                    Con la ayuda de{" "}
+                    <span className="font-extrabold">MindFuel</span>, mediante
+                    una buena planificación de objetivos alcanzables, la
+                    motivación y la disciplina se generan en el tiempo.
+                  </p>
+                )}
+                {screenTouches > 4 && (
+                  <p>
+                    Es un{" "}
+                    <span className="font-extrabold text-[#F57F11]">
+                      proceso
+                    </span>
+                    , las pequeñas acciones diarias son la llave secreta para el
+                    éxito.
+                  </p>
+                )}
+                {screenTouches > 5 && void Router.push("/signup")}
+              </div>
+              <div className="flex justify-center">
+                <Progress
+                  value={20 * (screenTouches - 1)}
+                  className="fixed bottom-7 w-[90%] min-[769px]:hidden"
+                />
+              </div>
             </div>
-          </div>
+          )}
         </div>
       </>
     );
