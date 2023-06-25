895195af4726d9dbbb6fc12d0f43f2e8a282ac65
dangoldmann <goldmandan8@gmail.com>
2023-06-25 20:40:20 -0300

    feat: Made responsive

--- src/components/auth/logo.tsx
+++ src/components/auth/logo.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Logo() {
   return (
-    <div className="flex w-full justify-end pr-5 xl:pr-10">
+    <div className="hidden w-full justify-end pr-5 min-[200px]:flex xl:pr-10 ">
       <Image alt="Logo" src="/favicon.ico" width={75} height={58}></Image>
     </div>
   );
