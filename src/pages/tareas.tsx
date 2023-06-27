b54ba29c8cadffc01aa72c5320152ddbc6426b3b
mindfuel-app <135031499+mindfuel-app@users.noreply.github.com>
2023-06-27 10:38:48 -0300

    feat: Added tabs component

--- src/pages/tareas.tsx
+++ src/pages/tareas.tsx
@@ -1,10 +1,36 @@
 import Layout from "~/components/layout";
-import Recommend from "~/components/ia";
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "../components/ui/tabs";
 
 export default function Tareas() {
   return (
     <Layout>
-      <Recommend />
+      <div className="flex h-full flex-col items-center bg-[#E9E9E9] px-6 py-12">
+        <Tabs defaultValue="tareas" className="w-full">
+          <div className="flex justify-center">
+            <TabsList>
+              <TabsTrigger value="tareas">Tareas</TabsTrigger>
+              <TabsTrigger value="rutinas">Rutinas</TabsTrigger>
+            </TabsList>
+          </div>
+          <TabsContent value="tareas">
+            <div className="flex flex-col">
+              <h2 className="my-5 font-medium">Tareas pendientes de hoy</h2>
+              <div className="bg-[#D9D9D9]"></div>
+            </div>
+          </TabsContent>
+          <TabsContent value="rutinas">
+            <div className="flex flex-col">
+              <h2 className="my-5 font-medium">Tus rutinas</h2>
+              <div className="bg-[#D9D9D9]"></div>
+            </div>
+          </TabsContent>
+        </Tabs>
+      </div>
     </Layout>
   );
 }
