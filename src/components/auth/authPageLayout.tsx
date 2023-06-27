1f919fba627f2a68bcd0ee3119579a6bb4ff3467
mindfuel-app <135031499+mindfuel-app@users.noreply.github.com>
2023-06-27 14:12:36 -0300

    feat: Changed breakpoint

--- src/components/auth/authPageLayout.tsx
+++ src/components/auth/authPageLayout.tsx
@@ -9,8 +9,8 @@ export default function authPageLayout({
 }) {
   return (
     <div className="flex h-screen overflow-y-hidden bg-[#008080]">
-      <div className="h-screen w-0 xl:w-1/6"></div>
-      <main className="flex min-h-screen w-full flex-col bg-[#edece7] py-10 xl:shadow-2xl">
+      <div className="h-screen w-0 min-[1440px]:w-1/6"></div>
+      <main className="flex min-h-screen w-full flex-col bg-[#edece7] py-10 min-[1440px]:shadow-2xl">
         <div className="flex flex-col items-center">
           <Logo />
           <div className="flex select-none flex-col items-center p-5">
@@ -21,7 +21,7 @@ export default function authPageLayout({
           </div>
         </div>
       </main>
-      <div className="h-screen w-0 xl:w-1/6"></div>
+      <div className="h-screen w-0 min-[1440px]:w-1/6"></div>
     </div>
   );
 }
