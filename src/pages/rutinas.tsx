e38a45c0ecc66ddfe4c6972f986cb7019e6a3020
FrancoSandri <46960579@est.ort.edu.ar>
2023-07-04 10:28:54 -0300

    ia en rutinas (sprint sandri)

--- src/pages/rutinas.tsx
+++ src/pages/rutinas.tsx
@@ -1,9 +1,11 @@
 import Layout from "~/components/layout";
+import Recommend from "~/components/ia"
 
 export default function Rutinas() {
   return (
     <Layout>
       <div>Rutinas</div>
+      <Recommend></Recommend>
     </Layout>
   );
 }
