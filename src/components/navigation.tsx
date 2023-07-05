ef6789801811d75cd3ce1d025d867940ba2ec7e0
mindfuel-app <135031499+mindfuel-app@users.noreply.github.com>
2023-07-05 11:47:49 -0300

    feat: Changed width

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -35,22 +35,28 @@ export default function Navigation() {
     <>
       <div className="no-highlight fixed bottom-0 w-full bg-white py-3">
         <div
-          className={`relative flex justify-center ${
+          className={`group relative flex justify-center ${
             router.pathname != "/home" ? "hidden" : ""
           }`}
         >
           <span className="absolute -top-10 h-16 w-16 rounded-full bg-[#EDECE7] active:-top-20"></span>
-          <Button className="absolute -top-9 h-14 w-14 rounded-full border-[3px] border-[#008080] bg-white">
-            <span className="text-2xl font-extrabold text-[#008080]">+</span>
+          <Button className="absolute -top-9 h-14 w-14 rounded-full border-[3px] border-[#008080] bg-white group-active:bg-[#008080]">
+            <span className="text-2xl font-extrabold text-[#008080] group-active:text-white">
+              +
+            </span>
           </Button>
         </div>
-        <div className="flex justify-between">
-          <div className="flex w-[180px] justify-evenly">
+        <div
+          className={`${
+            router.pathname == "/home" ? "flex justify-between" : "hidden"
+          }`}
+        >
+          <div className="flex w-[160px] justify-evenly">
             <Link
               href="/home"
               className={`${
                 router.pathname == "/home" ? "text-[#008080]" : ""
-              } flex w-24 flex-col items-center gap-1 rounded-xl p-1 text-center active:bg-gray-100`}
+              } flex w-[71px] flex-col items-center gap-1 rounded-xl p-1 text-center active:bg-gray-100`}
               onClick={() => {
                 void router.push("/home");
               }}
@@ -62,7 +68,7 @@ export default function Navigation() {
               href="/self-care"
               className={`${
                 router.pathname == "/self-care" ? "text-[#008080]" : ""
-              } flex w-24 flex-col items-center gap-1 rounded-xl p-1 text-center active:bg-gray-100`}
+              } flex w-[71px] flex-col items-center gap-1 rounded-xl p-1 text-center active:bg-gray-100`}
               onClick={() => {
                 void router.push("/self-care");
               }}
@@ -71,12 +77,12 @@ export default function Navigation() {
               <span className="text-sm font-medium">Self-care</span>
             </Link>
           </div>
-          <div className="flex w-[180px] justify-evenly">
+          <div className="flex w-[160px] justify-evenly">
             <Link
               href="/amigos"
               className={`${
                 router.pathname == "/amigos" ? "text-[#008080]" : ""
-              } flex w-24 flex-col items-center gap-1 rounded-xl p-1 text-center active:bg-gray-100`}
+              } flex w-[71px] flex-col items-center gap-1 rounded-xl p-1 text-center active:bg-gray-100`}
               onClick={() => {
                 void router.push("/amigos");
               }}
@@ -88,7 +94,7 @@ export default function Navigation() {
               href="/perfil"
               className={`${
                 router.pathname == "/perfil" ? "text-[#008080]" : ""
-              } flex w-24 flex-col items-center gap-1 rounded-xl p-1 text-center active:bg-gray-100`}
+              } flex w-[71px] flex-col items-center gap-1 rounded-xl p-1 text-center active:bg-gray-100`}
               onClick={() => {
                 void router.push("/perfil");
               }}
@@ -98,6 +104,30 @@ export default function Navigation() {
             </Link>
           </div>
         </div>
+        <ul
+          className={`${
+            router.pathname != "/home" ? "flex justify-around px-2" : "hidden"
+          }`}
+        >
+          {menus.map((item) => {
+            return (
+              <li key={item.name} className="w-[71px]">
+                <Link
+                  href={item.href}
+                  className={`${
+                    router.pathname == item.href ? "text-[#008080]" : ""
+                  } flex flex-col items-center gap-1 rounded-xl p-1 text-center active:bg-gray-100`}
+                  onClick={() => {
+                    void router.push(item.href);
+                  }}
+                >
+                  {item.icon}
+                  <span className="text-sm font-medium">{item.name}</span>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </>
   );
