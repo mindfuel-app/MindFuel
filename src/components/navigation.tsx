ffa8e0e4b833e6093b0426356ddab4a77e50aa91
dangoldmann <goldmandan8@gmail.com>
2023-07-07 15:57:39 -0300

    feat: Changed navigation

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -33,29 +33,6 @@ function NavigationItem({
 }
 
 export default function Navigation() {
-  const menus = [
-    {
-      name: "Home",
-      href: "/home",
-      icon: <AiFillHome className="text-3xl" />,
-    },
-    {
-      name: "Self-care",
-      href: "/self-care",
-      icon: <FaHandHoldingHeart className="text-3xl" />,
-    },
-    {
-      name: "Amigos",
-      href: "/amigos",
-      icon: <FaUserFriends className="text-3xl" />,
-    },
-    {
-      name: "Perfil",
-      href: "/perfil",
-      icon: <BsPersonCircle className="text-3xl" />,
-    },
-  ];
-
   const router = useRouter();
 
   return (
@@ -74,11 +51,15 @@ export default function Navigation() {
           </Button>
         </div>
         <div
-          className={`${
-            router.pathname == "/home" ? "flex justify-between" : "hidden"
+          className={`flex ${
+            router.pathname == "/home" ? "justify-between" : "justify-stretch"
           }`}
         >
-          <div className="flex w-[140px] justify-evenly min-[350px]:w-[150px] min-[375px]:w-[160px]">
+          <div
+            className={`flex w-[140px] justify-evenly min-[350px]:w-[150px] min-[375px]:w-[160px] ${
+              router.pathname != "/home" ? "-mr-2 flex-1" : ""
+            }`}
+          >
             <NavigationItem
               href="/home"
               icon={<AiFillHome className="text-3xl" />}
@@ -90,7 +71,11 @@ export default function Navigation() {
               name="Self-care"
             />
           </div>
-          <div className="flex w-[140px] justify-evenly min-[350px]:w-[150px] min-[375px]:w-[160px]">
+          <div
+            className={`flex w-[140px] justify-evenly min-[350px]:w-[150px] min-[375px]:w-[160px] ${
+              router.pathname != "/home" ? "-ml-2 flex-1" : ""
+            }`}
+          >
             <NavigationItem
               href="/amigos"
               icon={<FaUserFriends className="text-3xl" />}
@@ -103,30 +88,6 @@ export default function Navigation() {
             />
           </div>
         </div>
-        <ul
-          className={`${
-            router.pathname != "/home" ? "flex justify-around px-2" : "hidden"
-          }`}
-        >
-          {menus.map((item) => {
-            return (
-              <li key={item.name} className="w-[71px]">
-                <Link
-                  href={item.href}
-                  className={`${
-                    router.pathname == item.href ? "text-[#008080]" : ""
-                  } flex flex-col items-center gap-1 rounded-xl p-1 text-center active:bg-gray-100`}
-                  onClick={() => {
-                    void router.push(item.href);
-                  }}
-                >
-                  {item.icon}
-                  <span className="text-sm font-medium">{item.name}</span>
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
       </div>
     </>
   );
