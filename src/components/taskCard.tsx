f5d5aaa3d289c9b9e5e2661978e510ba33547fda
dangoldmann <goldmandan8@gmail.com>
2023-07-20 20:13:14 -0300

    feat: Added checkbox to taskcard

--- src/components/taskCard.tsx
+++ src/components/taskCard.tsx
@@ -1,3 +1,5 @@
+import { Checkbox } from "./ui/checkbox";
+
 export type Task = {
   id: number;
   numeroTarea: number;
@@ -16,8 +18,11 @@ export function TaskCard({
       <div className="w-[15%] rounded-l-md bg-teal p-2 text-center text-white">
         {numeroTarea}
       </div>
-      <div className="flex w-full items-center justify-start rounded-r-md border-2 border-teal bg-white pl-3">
+      <div className="relative flex w-full items-center justify-start rounded-r-md border-2 border-teal bg-white pl-3">
         <span className="text-black">{nombreTarea}</span>
+        <div className="group absolute -right-3 flex h-8 w-8 items-center justify-center rounded-full border-2 border-teal bg-white active:bg-gray-200">
+          <Checkbox className="no-highlight border-0 group-active:border-gray-200 group-active:bg-gray-200" />
+        </div>
       </div>
     </div>
   );
