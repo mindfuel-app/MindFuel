8c8db491104d5a5f525fe020fe4995eda3eb8227
dangoldmann <goldmandan8@gmail.com>
2023-07-22 19:12:33 -0300

    feat: Added hover state for laptop

--- src/components/taskCard.tsx
+++ src/components/taskCard.tsx
@@ -20,7 +20,7 @@ export function TaskCard({
       </div>
       <div className="relative flex w-full items-center justify-start rounded-r-md border-2 border-teal bg-white pl-3">
         <span className="text-black">{nombreTarea}</span>
-        <div className="group absolute -right-3 flex h-8 w-8 items-center justify-center rounded-full border-2 border-teal bg-white active:bg-gray-200">
+        <div className="group absolute -right-3 flex h-8 w-8 items-center justify-center rounded-full border-2 border-teal bg-white active:bg-gray-200 lg:hover:bg-gray-200">
           <Checkbox className="no-highlight border-0 group-active:border-gray-200 group-active:bg-gray-200" />
         </div>
       </div>
