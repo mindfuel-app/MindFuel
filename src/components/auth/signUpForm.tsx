24a57af24c46062007230ef3430ef1cd72094a67
dangoldmann <goldmandan8@gmail.com>
2023-08-01 13:33:51 -0300

    feat: Added styling on disabled form

--- src/components/auth/signUpForm.tsx
+++ src/components/auth/signUpForm.tsx
@@ -13,7 +13,7 @@ import Tooltip from "./tooltip";
 
 export default function SignUpForm() {
   const [PasswordInputType, ToggleIcon] = usePasswordToggle();
-  const [isButtonDisabled, setIsButtonDisabled] = useState(false);
+  const [isFormDisabled, setIsFormDisabled] = useState(false);
   const [isEmailWrong, setIsEmailWrong] = useState(false);
   const [isUsernameWrong, setIsUsernameWrong] = useState(false);
 
@@ -38,7 +38,7 @@ export default function SignUpForm() {
   });
 
   const submitData = async (data: FormData) => {
-    setIsButtonDisabled(true);
+    setIsFormDisabled(true);
     await signIn("credentials", {
       name: data.name,
       email: data.email,
@@ -47,7 +47,7 @@ export default function SignUpForm() {
       redirect: false,
     }).then((response) => {
       if (response?.error) {
-        setIsButtonDisabled(false);
+        setIsFormDisabled(false);
         if (response.error == "Este email ya esta registrado") {
           return setIsEmailWrong(true);
         }
@@ -62,106 +62,109 @@ export default function SignUpForm() {
 
   return (
     <form
-      className="flex flex-col justify-center space-y-7 py-3 sm:py-10"
+      className="py-3 sm:py-10"
       // eslint-disable-next-line @typescript-eslint/no-misused-promises
       onSubmit={handleSubmit(submitData)}
     >
-      <div className="flex flex-col gap-2">
-        <label className="flex flex-col">
-          <span
-            className={`ml-1 flex items-center gap-1 font-medium ${
-              isUsernameWrong ? "text-red-500" : ""
-            }`}
-            onClick={(e) => e.preventDefault()}
-          >
-            Nombre de usuario <Tooltip />
-          </span>
-          <div className="flex flex-col gap-2">
+      <fieldset
+        disabled={isFormDisabled}
+        className="group flex flex-col justify-center space-y-7"
+      >
+        <div className="flex flex-col gap-2 group-disabled:opacity-50">
+          <label className="flex flex-col group-disabled:cursor-not-allowed">
+            <span
+              className={`ml-1 flex items-center gap-1 font-medium ${
+                isUsernameWrong ? "text-red-500" : ""
+              }`}
+              onClick={(e) => e.preventDefault()}
+            >
+              Nombre de usuario <Tooltip />
+            </span>
             <input
               type="text"
-              className={`w-full rounded-xl border-2 border-teal px-3 py-1 outline-none ${
+              className={`w-full rounded-xl border-2 border-teal px-3 py-1 outline-none group-disabled:cursor-not-allowed ${
                 isUsernameWrong ? "border-red-500" : ""
               }`}
               {...register("name")}
             />
             {isUsernameWrong && (
-              <span className="absolute mt-10 text-xs text-red-500">
+              <span className="absolute mt-16 text-xs text-red-500">
                 Ya existe una cuenta con este nombre de usuario
               </span>
             )}
             {!isUsernameWrong && errors.name && (
-              <span className="absolute mt-10 text-xs text-red-500">
+              <span className="absolute mt-16 text-xs text-red-500">
                 Nombre de usuario: 3-30 caracteres
               </span>
             )}
-          </div>
-        </label>
-      </div>
-      <div className="flex flex-col gap-2">
-        <label className="flex flex-col">
-          <span
-            className={`ml-1 font-medium ${isEmailWrong ? "text-red-500" : ""}`}
-            onClick={(e) => e.preventDefault()}
-          >
-            Email
-          </span>
-          <div className="flex flex-col gap-2">
+          </label>
+        </div>
+        <div className="flex flex-col gap-2 group-disabled:opacity-50">
+          <label className="flex flex-col group-disabled:cursor-not-allowed">
+            <span
+              className={`ml-1 font-medium ${
+                isEmailWrong ? "text-red-500" : ""
+              }`}
+              onClick={(e) => e.preventDefault()}
+            >
+              Email
+            </span>
             <input
               type="text"
-              className={`w-full rounded-xl border-2 border-teal px-3 py-1 outline-none ${
+              className={`w-full rounded-xl border-2 border-teal px-3 py-1 outline-none group-disabled:cursor-not-allowed ${
                 isEmailWrong ? "border-red-500" : ""
               }`}
               {...register("email")}
             />
             {isEmailWrong && (
-              <span className="absolute mt-10 text-xs text-red-500">
+              <span className="absolute mt-16 text-xs text-red-500">
                 Ya existe una cuenta con este email
               </span>
             )}
             {!isEmailWrong && errors.email && (
-              <span className="absolute mt-10 text-xs text-red-500">
+              <span className="absolute mt-16 text-xs text-red-500">
                 Ingrese un email valido
               </span>
             )}
-          </div>
-        </label>
-      </div>
-      <div className="flex flex-col gap-2">
-        <label className="flex flex-col">
-          <span
-            className="ml-1 font-medium"
-            onClick={(e) => e.preventDefault()}
-          >
-            Contraseña
-          </span>
-          <div className="flex w-full items-center justify-between rounded-xl border-2 border-teal bg-white px-3 py-1">
-            <input
-              type={PasswordInputType == "text" ? "text" : "password"}
-              className="w-full outline-none"
-              {...register("password")}
-            />
-            <span>{ToggleIcon}</span>
-          </div>
-        </label>
-        {errors.password && (
-          <span className="absolute mt-16 text-xs text-red-500">
-            Contraseña: 6-30 caracteres
-          </span>
-        )}
-      </div>
-      <div className="flex w-full flex-col items-center gap-5">
-        <AuthButton
-          method="Sign up"
-          isDisabled={isButtonDisabled}
-          onClick={() => {
-            setIsEmailWrong(false);
-            setIsUsernameWrong(false);
-          }}
-        />
-      </div>
-      <Divider variant="middle">o</Divider>
-      <GoogleAuthShowcase />
-      <AlternativeMethodLink method="Sign in" />
+          </label>
+        </div>
+        <div className="flex flex-col gap-2 group-disabled:opacity-50">
+          <label className="flex flex-col group-disabled:cursor-not-allowed">
+            <span
+              className="ml-1 font-medium"
+              onClick={(e) => e.preventDefault()}
+            >
+              Contraseña
+            </span>
+            <div className="flex w-full items-center justify-between rounded-xl border-2 border-teal bg-white px-3 py-1">
+              <input
+                type={PasswordInputType == "text" ? "text" : "password"}
+                className="w-full outline-none group-disabled:cursor-not-allowed"
+                {...register("password")}
+              />
+              <span>{ToggleIcon}</span>
+            </div>
+          </label>
+          {errors.password && (
+            <span className="absolute mt-16 text-xs text-red-500">
+              Contraseña: 6-30 caracteres
+            </span>
+          )}
+        </div>
+        <div className="flex w-full flex-col items-center gap-5 pt-2">
+          <AuthButton
+            method="Sign up"
+            isDisabled={isFormDisabled}
+            onClick={() => {
+              setIsEmailWrong(false);
+              setIsUsernameWrong(false);
+            }}
+          />
+        </div>
+        <Divider variant="middle">o</Divider>
+        <GoogleAuthShowcase />
+        <AlternativeMethodLink method="Sign in" />
+      </fieldset>
     </form>
   );
 }
