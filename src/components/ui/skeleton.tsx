c361eb6893a7f59d823ceb9c9021d09e5fa3e314
dangoldmann <goldmandan8@gmail.com>
2023-08-03 18:56:42 -0300

    feat: Made skeleton larger

--- src/components/ui/skeleton.tsx
+++ src/components/ui/skeleton.tsx
@@ -4,7 +4,7 @@ export function TaskSkeleton() {
   return (
     <div className="flex w-full flex-col items-center gap-5 p-10">
       <div className="flex gap-2">
-        <Rectangle className="w-48" />
+        <Rectangle className="w-52" />
         <Circle />
       </div>
       <Rectangle />
@@ -16,7 +16,7 @@ function Rectangle({ className }: React.HTMLAttributes<HTMLDivElement>) {
   return (
     <div
       className={cn(
-        "h-10 w-60 animate-pulse rounded-md bg-gray-300",
+        "h-10 w-64 animate-pulse rounded-md bg-gray-300",
         className
       )}
     ></div>
