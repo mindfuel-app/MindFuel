2326ea447470c41fc354f335b8f1bd30e6e4e1b2
dangoldmann <goldmandan8@gmail.com>
2023-08-04 15:05:56 -0300

    feat: Created addModal component

---
+++ src/components/addModal.tsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import { Button } from "./ui/button";
+
+export default function AddModal({
+  TaskForm,
+  RoutineForm,
+}: {
+  TaskForm: JSX.Element;
+  RoutineForm: JSX.Element;
+}) {
+  const [selectedTab, setSelectedTab] = useState<"tareas" | "rutinas">(
+    "tareas"
+  );
+
+  return (
+    <div>
+      <div className="no-highlight relative flex w-full">
+        <Button
+          className={`relative h-14 w-1/2 text-xl active:bg-gray-100 ${
+            selectedTab == "tareas"
+              ? "font-medium text-gray-800"
+              : "text-gray-500"
+          }`}
+          onClick={() => setSelectedTab("tareas")}
+        >
+          Tareas
+          {selectedTab == "tareas" && (
+            <div className="absolute bottom-[1px] w-[50%] rounded-full border-[2px] border-teal"></div>
+          )}
+        </Button>
+        <Button
+          className={`relative h-14 w-1/2 text-xl active:bg-gray-100 ${
+            selectedTab == "rutinas"
+              ? "font-medium text-gray-800"
+              : "text-gray-500"
+          }`}
+          onClick={() => setSelectedTab("rutinas")}
+        >
+          Rutinas
+          {selectedTab == "rutinas" && (
+            <div className="absolute bottom-[1px] w-[50%] rounded-full border-[2px] border-teal"></div>
+          )}
+        </Button>
+        <div className="absolute bottom-0 w-full border-[1px] border-gray-100"></div>
+      </div>
+      {selectedTab == "tareas" ? TaskForm : RoutineForm}
+    </div>
+  );
+}
