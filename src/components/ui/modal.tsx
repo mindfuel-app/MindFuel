7db5b2bfb733b8f3139cb80d98bfec8272ffc98a
dangoldmann <goldmandan8@gmail.com>
2023-08-04 15:05:45 -0300

    feat: Deleted title and cross button

--- src/components/ui/modal.tsx
+++ src/components/ui/modal.tsx
@@ -1,4 +1,3 @@
-import { XMarkIcon } from "@heroicons/react/24/outline";
 import * as Dialog from "@radix-ui/react-dialog";
 import { type ReactNode } from "react";
 
@@ -18,23 +17,11 @@ export default function Modal({
   );
 }
 
-function ModalContent({
-  title,
-  children,
-}: {
-  title: string;
-  children: ReactNode;
-}) {
+function ModalContent({ children }: { children: ReactNode }) {
   return (
     <Dialog.Portal>
       <Dialog.Overlay className="fixed inset-0 z-30 bg-black/50 data-[state=closed]:animate-[dialog-overlay-hide_200ms] data-[state=open]:animate-[dialog-overlay-show_200ms]" />
-      <Dialog.Content className="fixed left-1/2 top-1/2 z-40 w-full max-w-[85%] -translate-x-1/2 -translate-y-1/2 rounded-md bg-white p-5 shadow transition-transform data-[state=closed]:animate-[dialog-content-hide_200ms] data-[state=open]:animate-[dialog-content-show_200ms] min-[500px]:max-w-md">
-        <div className="flex items-center justify-between">
-          <Dialog.Title className="text-xl">{title}</Dialog.Title>
-          <Dialog.Close className="no-highlight">
-            <XMarkIcon className="h-6 w-6 text-gray-500 hover:text-gray-600" />
-          </Dialog.Close>
-        </div>
+      <Dialog.Content className="fixed left-1/2 top-1/2 z-40 w-full max-w-[85%] -translate-x-1/2 -translate-y-1/2 rounded-md bg-white shadow transition-transform data-[state=closed]:animate-[dialog-content-hide_200ms] data-[state=open]:animate-[dialog-content-show_200ms] min-[500px]:max-w-md">
         {children}
       </Dialog.Content>
     </Dialog.Portal>
