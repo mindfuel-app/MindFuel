8bffc777340482be4b4837e833eac4ecba35e402
dangoldmann <goldmandan8@gmail.com>
2023-08-06 19:07:07 -0300

    feat: Added navigation to side menu

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,7 @@
 import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import { Button } from "./ui/button";
-import { XMarkIcon, Bars3Icon } from "@heroicons/react/24/outline";
+import { Bars3Icon } from "@heroicons/react/24/outline";
 import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import router from "next/router";
@@ -9,6 +9,10 @@ import Modal from "./ui/modal";
 import RoutineForm from "./routineForm";
 import AddModal from "./addModal";
 import TaskForm from "./taskForm";
+import { FaUserFriends, FaHandHoldingHeart } from "react-icons/fa";
+import { AiFillHome } from "react-icons/ai";
+import { BsPersonCircle } from "react-icons/bs";
+import { ArrowLeftOnRectangleIcon } from "@heroicons/react/24/outline";
 
 export default function ProfileInfo() {
   const { data: sessionData } = useSession();
@@ -26,7 +30,7 @@ export default function ProfileInfo() {
     );
 
   return (
-    <div className="flex items-center justify-between bg-alabaster p-3 font-medium">
+    <div className="flex items-center justify-between bg-alabaster p-3 pt-5 font-medium">
       <span>
         Bienvenido,{" "}
         <span className="font-semibold text-orange">
@@ -34,26 +38,28 @@ export default function ProfileInfo() {
         </span>
       </span>
       <Navigation />
-      <div className="flex gap-6">
-        <Modal open={isModalOpen} onOpenChange={setIsModalOpen}>
-          <Modal.Button className="no-highlight hidden items-center rounded-lg bg-teal px-3 py-1 text-white transition active:bg-teal/80 lg:flex">
-            Crear rutina
-          </Modal.Button>
-          <Modal.Content>
-            <AddModal
-              TaskForm={<TaskForm afterSave={() => setIsModalOpen(false)} />}
-              RoutineForm={
-                <RoutineForm afterSave={() => setIsModalOpen(false)} />
-              }
-            />
-          </Modal.Content>
-        </Modal>
-        <Button
-          className="no-highlight hidden p-2 min-[360px]:flex"
-          onClick={() => setIsSideMenuOpen(true)}
-        >
-          <Bars3Icon className="h-5 w-5 text-black" />
-        </Button>
+      <div className="fixed right-0 pr-3">
+        <div className="flex gap-3 xl:gap-6">
+          <Modal open={isModalOpen} onOpenChange={setIsModalOpen}>
+            <Modal.Button className="no-highlight hidden items-center rounded-lg bg-teal px-3 py-1 text-white transition active:bg-teal/80 lg:flex">
+              Crear rutina
+            </Modal.Button>
+            <Modal.Content>
+              <AddModal
+                TaskForm={<TaskForm afterSave={() => setIsModalOpen(false)} />}
+                RoutineForm={
+                  <RoutineForm afterSave={() => setIsModalOpen(false)} />
+                }
+              />
+            </Modal.Content>
+          </Modal>
+          <Button
+            className="no-highlight hidden p-2 min-[360px]:flex"
+            onClick={() => setIsSideMenuOpen(true)}
+          >
+            <Bars3Icon className="h-5 w-5 text-black" />
+          </Button>
+        </div>
       </div>
 
       <AnimatePresence>
@@ -65,14 +71,26 @@ export default function ProfileInfo() {
   );
 }
 
-function Navigation() {
-  const navigationItems = [
-    { href: "/home", label: "Home" },
-    { href: "/self-care", label: "Self-care" },
-    { href: "/amigos", label: "Amigos" },
-    { href: "/perfil", label: "Perfil" },
-  ];
+const navigationItems = [
+  { href: "/home", label: "Home", icon: <AiFillHome className="h-6 w-6" /> },
+  {
+    href: "/self-care",
+    label: "Self-care",
+    icon: <FaHandHoldingHeart className="h-6 w-6" />,
+  },
+  {
+    href: "/amigos",
+    label: "Amigos",
+    icon: <FaUserFriends className="h-6 w-6" />,
+  },
+  {
+    href: "/perfil",
+    label: "Perfil",
+    icon: <BsPersonCircle className="h-6 w-6" />,
+  },
+];
 
+function Navigation() {
   return (
     <div className="fixed left-1/2 mt-2 hidden -translate-x-1/2 gap-10 lg:flex xl:gap-16">
       {navigationItems.map((item) => (
@@ -104,18 +122,60 @@ function Navigation() {
 
 function SideMenu({ closeMenu }: { closeMenu: () => void }) {
   return (
-    <motion.div className="absolute right-0 top-0 z-30 w-36 bg-white px-3 py-5 shadow-md">
-      <div className="flex w-full justify-end">
-        <XMarkIcon className="h-5 w-5 cursor-pointer" onClick={closeMenu} />
-      </div>
-      <div className="flex flex-col py-12">
-        <Button
-          className="no-highlight p-0 font-normal text-black"
-          onClick={() => void signOut({ callbackUrl: "/signin" })}
-        >
-          Cerrar sesión
-        </Button>
-      </div>
-    </motion.div>
+    <>
+      <div
+        onClick={closeMenu}
+        className="fixed inset-0 z-30 bg-black/50 data-[state=closed]:animate-[dialog-overlay-hide_200ms] data-[state=open]:animate-[dialog-overlay-show_200ms]"
+      />
+      <motion.div
+        initial={{ width: 0 }}
+        animate={{
+          width: screen.width < 640 ? 224 : screen.width < 1024 ? 256 : 288,
+        }}
+        exit={{ x: screen.width < 640 ? 224 : screen.width < 1024 ? 256 : 288 }}
+        transition={{ duration: 0.4 }}
+        className="absolute right-0 top-0 z-30 h-full w-56 bg-white py-5 sm:w-64 lg:w-72"
+      >
+        <div className="pb-5 pt-3 text-center text-2xl text-teal">
+          Mind
+          <span className="text-orange">Fuel</span>
+        </div>
+        <div className="w-full border-[1px] border-gray-300" />
+        <div className="flex flex-col py-3">
+          {navigationItems.map((item) => (
+            <Link
+              href={item.href}
+              key={item.label}
+              className={`no-highlight rounded-r-full px-5 py-2 transition-colors active:bg-gray-200 ${
+                router.pathname == item.href ? "bg-teal/30" : ""
+              }`}
+            >
+              <div className="flex items-center gap-4">
+                {item.icon}
+                <span
+                  className={`text-lg ${
+                    router.pathname == item.href ? "font-medium" : "font-normal"
+                  }`}
+                >
+                  {item.label}
+                </span>
+              </div>
+            </Link>
+          ))}
+        </div>
+        <div className="w-full border-[1px] border-gray-300" />
+        <div className="flex flex-col py-5">
+          <Button
+            className="no-highlight p-0 text-lg font-normal text-black"
+            onClick={() => void signOut({ callbackUrl: "/signin" })}
+          >
+            <div className="flex items-center gap-3">
+              <ArrowLeftOnRectangleIcon className="h-6 w-6" />
+              <span>Cerrar sesión</span>
+            </div>
+          </Button>
+        </div>
+      </motion.div>
+    </>
   );
 }
