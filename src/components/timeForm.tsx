d0ee443306bd0ea1d90f064718519612bdda27c1
mindfuel-app <135031499+mindfuel-app@users.noreply.github.com>
2023-08-08 13:42:56 -0300

    feat: Changed time form

--- src/components/timeForm.tsx
+++ src/components/timeForm.tsx
@@ -1,12 +1,17 @@
 import { XMarkIcon, CheckIcon } from "@heroicons/react/24/outline";
+import { motion } from "framer-motion";
 import { Button } from "./ui/button";
 
-export default function TimeModal({ afterSave }: { afterSave: () => void }) {
+export default function TimeForm({ afterSave }: { afterSave: () => void }) {
   return (
-    <div className="p-3">
+    <motion.div
+      initial={{ opacity: 0.5, scale: 0.95 }}
+      animate={{ opacity: 1, scale: 1 }}
+      className="p-5"
+    >
       <div className="flex w-full justify-end">
         <XMarkIcon
-          className="h-5 w-5"
+          className="h-5 w-5 cursor-pointer"
           onClick={(e) => {
             e.preventDefault();
             afterSave();
@@ -22,6 +27,6 @@ export default function TimeModal({ afterSave }: { afterSave: () => void }) {
           <CheckIcon className="h-16 w-16" />
         </Button>
       </div>
-    </div>
+    </motion.div>
   );
 }
