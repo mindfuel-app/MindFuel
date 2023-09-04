a3911ea999c1ee2349c4d62ba25c594bcd5a5f55
dangoldmann <goldmandan8@gmail.com>
2023-09-04 19:39:26 -0300

    feat: Added swipe handlers to home tabs

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,5 @@
 import Layout from "~/components/layout";
-import {
-  Tabs,
-  TabsContent,
-  TabsList,
-  TabsTrigger,
-} from "../components/ui/tabs";
+import { Tabs, TabsList } from "../components/ui/tabs";
 import Head from "next/head";
 import { useState } from "react";
 import { motion } from "framer-motion";
@@ -12,6 +7,7 @@ import TaskList from "../components/task/taskList";
 import RoutineList from "../components/routine/routineList";
 import { useSession } from "next-auth/react";
 import Router from "next/router";
+import useSwipe from "~/hooks/useSwipe";
 
 const tabOptions = [
   { value: "tareas", label: "Tareas" },
@@ -21,6 +17,18 @@ const tabOptions = [
 export default function Home() {
   const [selectedTab, setSelectedTab] = useState("tareas");
   const { data: sessionData, status } = useSession();
+
+  const swipeLeftHandler = useSwipe({
+    onSwipedLeft: () => {
+      setSelectedTab("rutinas");
+    },
+  });
+
+  const swipeRightHandler = useSwipe({
+    onSwipedRight: () => {
+      setSelectedTab("tareas");
+    },
+  });
 
   if (status == "unauthenticated") return void Router.push("/signin");
 
@@ -46,18 +54,22 @@ export default function Home() {
         >
           Probar Notificacion
         </button>
-        <Tabs defaultValue="tareas" className="h-full w-full">
+        <Tabs className="h-screen w-full">
           <div className="mt-5 flex justify-center">
             <TabsList>
               {tabOptions.map((tab) => (
-                <TabsTrigger
+                <button
                   key={tab.value}
-                  value={tab.value}
-                  className="relative w-1/2"
+                  className={`relative inline-flex w-1/2 select-none items-center justify-center whitespace-nowrap rounded-sm px-5 py-1.5 text-lg font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50
+                  ${
+                    selectedTab == tab.value
+                      ? "text-foreground text-white shadow-sm"
+                      : ""
+                  }
+                  `}
                   onClick={() => setSelectedTab(tab.value)}
                 >
                   <div className="z-20">{tab.label}</div>
-
                   {tab.value == selectedTab && (
                     <motion.div
                       layoutId="bg"
@@ -69,16 +81,20 @@ export default function Home() {
                       className="absolute left-0 top-0 z-10 h-full w-full rounded-sm bg-teal"
                     ></motion.div>
                   )}
-                </TabsTrigger>
+                </button>
               ))}
             </TabsList>
           </div>
-          <TabsContent value="tareas" className="h-full">
-            <TaskList />
-          </TabsContent>
-          <TabsContent value="rutinas" className="h-full">
-            <RoutineList />
-          </TabsContent>
+          {selectedTab == "tareas" && (
+            <div {...swipeLeftHandler} className="h-full">
+              <TaskList />
+            </div>
+          )}
+          {selectedTab == "rutinas" && (
+            <div {...swipeRightHandler} className="h-full">
+              <RoutineList />
+            </div>
+          )}
         </Tabs>
       </Layout>
     </>
