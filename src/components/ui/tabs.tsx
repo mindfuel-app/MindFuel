bfb46ed65e65b6efb98629076ade6e63c8637c0c
dangoldmann <goldmandan8@gmail.com>
2023-09-04 19:38:08 -0300

    feat: Formatted styles

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -15,7 +15,7 @@ const TabsList = React.forwardRef<
   <TabsPrimitive.List
     ref={ref}
     className={cn(
-      "no-highlight inline-flex  items-center justify-center rounded-md bg-white p-1 text-muted-foreground",
+      "no-highlight inline-flex items-center justify-center rounded-md bg-white p-1 text-muted-foreground",
       className
     )}
     {...props}
@@ -30,7 +30,7 @@ const TabsTrigger = React.forwardRef<
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
-      "inline-flex select-none items-center justify-center whitespace-nowrap rounded-sm px-5 py-1.5 text-lg font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:text-foreground  data-[state=active]:text-white data-[state=active]:shadow-sm",
+      "inline-flex select-none items-center justify-center whitespace-nowrap rounded-sm px-5 py-1.5 text-lg font-medium ring-offset-background transition-all data-[state=active]:text-foreground data-[state=active]:text-white data-[state=active]:shadow-sm focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2  disabled:pointer-events-none disabled:opacity-50",
       className
     )}
     {...props}
