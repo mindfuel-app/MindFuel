a48f0f615f4084a6ec50009d581294df67fc85a7
FrancoSandri <46960579@est.ort.edu.ar>
2023-09-11 13:21:14 -0300

    chatbot app done

--- ia/chatbot.py
+++ ia/chatbot.py
@@ -1,38 +1,44 @@
 import openai
 from flask import Flask, request, jsonify
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-# Reemplaza con tu clave de API
-openai.api_key = "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"
+openai.api_type = "azure"
+openai.api_base = "https://mindfuel.openai.azure.com/"
+openai.api_version = "2023-07-01-preview"
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 app = Flask(__name__)
 
 def dividir_tarea_en_pasos(prompt):
-    # Crear una solicitud para dividir la tarea en pasos
     solicitud = [
         {"role": "user", "content": prompt},
-        {"role": "assistant", "content": "Divide la tarea en pasos más simples:"}
+        {"role": "assistant", "content": "Divide la tarea en 3 pasos más simples y cortos, sin explayarte tanto:"}
     ]
     
-    # Utilizar la API de OpenAI para obtener una respuesta
-    response = openai.Completion.create(
-        model="gpt-3.5-turbo",
+    response = openai.ChatCompletion.create(
+        engine="mindfuel",
         messages=solicitud,
-        max_tokens=50  # Puedo ajustar este valor para obtener más o menos pasos
+        temperature=0.7,
+        max_tokens=200,
+        top_p=0.95,
+        frequency_penalty=0,
+        presence_penalty=0,
+        stop=None
     )
-    
-    # Extraer los pasos sugeridos del resultado
-    pasos = [message['content'] for message in response['choices'][0]['message']['messages'] if message['role'] == 'assistant']
-    
-    return pasos
+
+    respuesta = response['choices'][0]['message']['content']
+    return respuesta
 
 @app.route('/dividir_tarea', methods=['POST'])
 def procesar_tarea():
     data = request.get_json()
     tarea = data.get('tarea')
-    
-    pasos = dividir_tarea_en_pasos(tarea)
-    
-    return jsonify(pasos)
+
+    respuesta = dividir_tarea_en_pasos(tarea)
+
+    return jsonify({"respuesta": respuesta})
 
 if __name__ == '__main__':
     app.run()
