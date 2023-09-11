ae8ddfea2ebaa9d71cca736a9f2cc12fae993ceb
IanPol0 <ipolonuer@gmail.com>
2023-09-11 15:29:51 -0300

    need to figure out how to use web-push wit tRPC

--- src/server/api/routers/pushSuscriptions.ts
+++ src/server/api/routers/pushSuscriptions.ts
@@ -1,6 +1,18 @@
-import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
+import { createTRPCRouter, protectedProcedure, publicProcedure } from "~/server/api/trpc";
 import { z } from "zod";
-import { error } from "console";
+import "web-push" // Typescript doesn't recognize web-push as a module, so we need to import it like this
+// import webpush from 'web-push';
+import { env } from "~/env.mjs";
+
+const vapidKeys = {
+  publicKey: env.PUBLIC_VAPID,
+  privateKey: env.PRIVATE_VAPID
+};
+// webpush.setVapidDetails(
+//   'mailto:proyectotdahort@gmail.com',
+//   vapidKeys.publicKey,
+//   vapidKeys.privateKey,
+// );
 
 export const pushRouter = createTRPCRouter({
   addPush: protectedProcedure
@@ -16,4 +28,23 @@ export const pushRouter = createTRPCRouter({
           });
       return pushSubscription;
     }),
+
+    // sendPushToOne: publicProcedure
+    // .input(z.object({ user_id: z.string(), title: z.string(), body: z.string() }))
+    // .mutation(async ({ ctx, input }) => {
+    //   const pushSubscriptions = await ctx.prisma.pushSubscription.findMany({
+    //     where: {
+    //       user_id: input.user_id
+    //     }});
+      
+    //   pushSubscriptions.forEach(pushSubscription => {
+    //     const payload = JSON.stringify({
+    //       title: input.title,
+    //       body: input.body,
+    //     });
+    //     webpush.sendNotification(pushSubscription.suscription, payload).catch((error) => console.error(error));
+    //   });
+    //   return pushSubscriptions;
+    // }),
+
   });
