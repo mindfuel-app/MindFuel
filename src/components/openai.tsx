f194063bbf19ae35fba9cd26374a65ec35162ec5
FrancoSandri <46960579@est.ort.edu.ar>
2023-09-11 15:08:41 -0300

    fetch openai api

---
+++ src/components/openai.tsx
@@ -0,0 +1,25 @@
+interface DivisionTareaResponse {
+  respuesta: string[];
+}
+
+export async function obtenerListaDePasos(tarea: string): Promise<string[]> {
+  try {
+    const response = await fetch("http://127.0.0.1:5000/dividir_tarea", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ tareas: tarea }),
+    });
+
+    if (response.ok) {
+      const data: DivisionTareaResponse = await response.json();
+      return data.respuesta;
+    } else {
+      throw new Error("Error al procesar la solicitud.");
+    }
+  } catch (error) {
+    console.error(error);
+    throw new Error("Error de red o del servidor.");
+  }
+}
