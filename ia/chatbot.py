a119526475d00d39a53c641ffa855658e36ddf81
mindfuel-app <135031499+mindfuel-app@users.noreply.github.com>
2023-09-12 09:58:23 -0300

    try fix on cors

--- ia/chatbot.py
+++ ia/chatbot.py
@@ -2,7 +2,7 @@ import openai
 from flask import Flask, request, jsonify
 import os
 from dotenv import load_dotenv
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 import re
 load_dotenv()
 
@@ -13,8 +13,9 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 app = Flask(__name__)
 CORS(app, origins=["http://localhost:3000"], supports_credentials=True)
-
 @app.route("/")
+@cross_origin(origin='*')
+
 def home():
     return "hello world"
 
