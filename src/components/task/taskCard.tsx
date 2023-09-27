38ea189654db3ce02ddca7897d03ebc215f86fa7
mindfuel-app <135031499+mindfuel-app@users.noreply.github.com>
2023-09-27 08:12:16 -0300

    feat: Minor changes

--- src/components/task/taskCard.tsx
+++ src/components/task/taskCard.tsx
@@ -33,71 +33,69 @@ export default function TaskCard({
             opacity: 0,
             x: 20,
           }}
-          className="flex w-full"
+          className="relative flex w-full items-center justify-start rounded-md border-2 border-teal bg-white py-1 pl-3"
         >
-          <div className="relative flex w-full items-center justify-start rounded-md border-2 border-teal bg-white py-1 pl-3">
-            <div className="flex flex-col py-1">
-              <span
-                className={`text-black ${
-                  (isChecked && showCompletedTasks) ||
-                  (isTaskDone && showCompletedTasks)
-                    ? "line-through opacity-50"
-                    : ""
-                }`}
-              >
-                {name}
-              </span>
-              {deadlineDate && (
-                <div className="flex gap-1">
-                  <CalendarIcon className="mt-[1px] h-4 w-4 text-orange" />
-                  <span
-                    className={`text-sm text-orange ${
-                      isTaskDone ? "text-gray-500 line-through" : ""
-                    }`}
-                  >
-                    {deadlineDate}
-                  </span>
-                </div>
-              )}
-            </div>
-            {!isTaskDone && (
-              <motion.button
-                initial={{ scale: 0.9, opacity: 0 }}
-                animate={{ scale: 1, opacity: 1 }}
-                className="no-highlight group absolute right-6"
-              >
-                <PencilSquareIcon className="h-7 w-7 text-gray-600 group-active:text-gray-800 lg:group-hover:text-gray-800" />
-              </motion.button>
-            )}
-            <div
-              onClick={() => {
-                setShowCheck(!showCheck);
-                setTimeout(() => {
-                  if (showCheck) {
-                    setIsTaskDone(false);
-                    setTaskUndone({ tasks: [id] });
-                  } else {
-                    setIsTaskDone(true);
-                    setTaskDone({ task_id: id, realTime: 0 });
-                  }
-                }, 250);
-              }}
-              className="no-highlight group absolute right-1 flex h-8 w-8 translate-x-1/2 cursor-pointer items-center justify-center rounded-full border-2 border-teal bg-white"
+          <div className="flex flex-col py-1">
+            <span
+              className={`text-black ${
+                (isChecked && showCompletedTasks) ||
+                (isTaskDone && showCompletedTasks)
+                  ? "line-through opacity-50"
+                  : ""
+              }`}
             >
-              {showCheck && (
-                <motion.div
-                  initial={{ scale: 0 }}
-                  animate={{ scale: 1 }}
-                  transition={{
-                    type: "spring",
-                    stiffness: 260,
-                    damping: 20,
-                  }}
+              {name}
+            </span>
+            {deadlineDate && (
+              <div className="flex gap-1">
+                <CalendarIcon className="mt-[1px] h-4 w-4 text-orange" />
+                <span
+                  className={`text-sm text-orange ${
+                    isTaskDone ? "text-gray-500 line-through" : ""
+                  }`}
                 >
-                  <CheckIcon className="h-5 w-5 text-teal" />
-                </motion.div>
-              )}
-            </div>
+                  {deadlineDate}
+                </span>
+              </div>
+            )}
+          </div>
+          {!isTaskDone && (
+            <motion.button
+              initial={{ scale: 0.9, opacity: 0 }}
+              animate={{ scale: 1, opacity: 1 }}
+              className="no-highlight group absolute right-6"
+            >
+              <PencilSquareIcon className="h-7 w-7 text-gray-600 group-active:text-gray-800 lg:group-hover:text-gray-800" />
+            </motion.button>
+          )}
+          <div
+            onClick={() => {
+              setShowCheck(!showCheck);
+              setTimeout(() => {
+                if (showCheck) {
+                  setIsTaskDone(false);
+                  setTaskUndone({ tasks: [id] });
+                } else {
+                  setIsTaskDone(true);
+                  setTaskDone({ task_id: id, realTime: 0 });
+                }
+              }, 250);
+            }}
+            className="no-highlight group absolute right-1 flex h-8 w-8 translate-x-1/2 cursor-pointer items-center justify-center rounded-full border-2 border-teal bg-white"
+          >
+            {showCheck && (
+              <motion.div
+                initial={{ scale: 0 }}
+                animate={{ scale: 1 }}
+                transition={{
+                  type: "spring",
+                  stiffness: 260,
+                  damping: 20,
+                }}
+              >
+                <CheckIcon className="h-5 w-5 text-teal" />
+              </motion.div>
+            )}
           </div>
         </motion.div>
       )}
