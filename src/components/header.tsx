a58c76d57118f816f38f730cbafad644ba3ef208
dangoldmann <goldmandan8@gmail.com>
2023-10-01 11:20:42 -0300

    feat: Added logout button

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,6 @@
-import { useSession } from "next-auth/react";
+import { useSession, signOut } from "next-auth/react";
 import Link from "next/link";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import router from "next/router";
 import Modal from "./ui/modal";
 import RoutineForm from "./routine/routineForm";
@@ -10,11 +10,19 @@ import { FaUserFriends, FaHandHoldingHeart } from "react-icons/fa";
 import { AiFillHome } from "react-icons/ai";
 import { BsPersonCircle } from "react-icons/bs";
 import { useSearchParams, useRouter } from "next/navigation";
+import {
+  ArrowLeftOnRectangleIcon,
+  ChevronDownIcon,
+} from "@heroicons/react/24/outline";
+import { useState } from "react";
+import { Button } from "./ui/button";
 
 export default function ProfileInfo() {
   const router = useRouter();
+  const [showLogout, setShowLogout] = useState(false);
   const { data: sessionData } = useSession();
   const searchParams = useSearchParams();
+
   const tabParam =
     searchParams.get("tab") == "tareas" || searchParams.get("tab") == "rutinas"
       ? (searchParams.get("tab") as string)
@@ -37,12 +45,43 @@ export default function ProfileInfo() {
 
   return (
     <div className="flex items-center justify-between bg-alabaster p-3 pt-5 font-medium">
-      <span>
-        Bienvenido,{" "}
-        <span className="font-semibold text-orange">
-          {sessionData.user.name}
+      <div className="relative flex items-center gap-1">
+        <span>
+          Bienvenido,{" "}
+          <span className="font-semibold text-orange">
+            {sessionData.user.name}
+          </span>
         </span>
-      </span>
+        <motion.div
+          initial={{ rotate: 0 }}
+          animate={{ rotate: showLogout ? 180 : 0 }}
+          transition={{ duration: 0.2 }}
+          onClick={() => setShowLogout(!showLogout)}
+        >
+          <ChevronDownIcon className="h-4 w-4 text-gray-600 active:text-gray-900" />
+        </motion.div>
+        <AnimatePresence>
+          {showLogout && (
+            <motion.div
+              initial={{ opacity: 0, scale: 0.95 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 0.95 }}
+              transition={{ duration: 0.2 }}
+              className="absolute -right-2 top-7 z-20 rounded-lg bg-gray-700 px-2 text-white shadow-lg"
+            >
+              <Button
+                className="no-highlight p-0 text-base font-normal"
+                onClick={() => void signOut({ callbackUrl: "/signin" })}
+              >
+                <div className="flex items-center gap-2">
+                  <ArrowLeftOnRectangleIcon className="h-5 w-5" />
+                  <span>Cerrar sesión</span>
+                </div>
+              </Button>
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </div>
       <Navigation />
       <Modal open={isModalOpen} onOpenChange={setIsModalOpen}>
         <Modal.Button className="no-highlight fixed right-7 hidden rounded-lg bg-teal px-3 py-2 text-white transition-all active:bg-teal/80 lg:block">
