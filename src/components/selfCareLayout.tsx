aacb1d8dacb3bd72c67d6e91348e2226f2755b35
dangoldmann <goldmandan8@gmail.com>
2023-10-01 22:04:45 -0300

    feat: Created back button component

--- src/components/selfCareLayout.tsx
+++ src/components/selfCareLayout.tsx
@@ -1,10 +1,9 @@
 import router from "next/router";
 import { type Session } from "next-auth";
 import Head from "next/head";
-import Link from "next/link";
-import { MdArrowBackIosNew } from "react-icons/md";
 import { BottomNavigation, TopNavigation } from "~/components/navigation";
 import { UserProvider } from "~/lib/UserContext";
+import BackButton from "./backButton";
 
 export default function SelfCareLayout({
   children,
@@ -44,11 +43,7 @@ function Header() {
         isInMenus ? "justify-end" : "justify-between"
       }`}
     >
-      {!isInMenus && (
-        <Link href="/self-care" className="no-highlight active:scale-95">
-          <MdArrowBackIosNew className="text-2xl text-teal" />
-        </Link>
-      )}
+      {!isInMenus && <BackButton href="/self-care" color="teal" />}
       <TopNavigation />
       <span className="text-lg font-medium text-teal">Self-care</span>
       <div className="absolute bottom-0 left-0 w-full border-[3px] border-teal lg:hidden" />
