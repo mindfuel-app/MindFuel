ae15ff362a52e9bc01681f9fcf4c10e234ae8e02
mindfuel-app <135031499+mindfuel-app@users.noreply.github.com>
2023-10-03 10:40:25 -0300

    feat: Created profile layout

---
+++ src/components/profileLayout.tsx
@@ -0,0 +1,33 @@
+import { type Session } from "next-auth";
+import Head from "next/head";
+import { UserProvider } from "~/lib/UserContext";
+import { BottomNavigation } from "./navigation";
+
+export default function ProfileLayout({
+  header,
+  children,
+  sessionData,
+}: {
+  header: JSX.Element;
+  children: React.ReactNode;
+  sessionData: Session;
+}) {
+  return (
+    <>
+      <Head>
+        <title>{sessionData.user.name}</title>
+      </Head>
+      <UserProvider
+        id={sessionData.user.id}
+        name={sessionData.user.name || ""}
+        email={sessionData.user.email || ""}
+      >
+        <div className="flex h-screen flex-col">
+          {header}
+          <main className="flex h-full flex-col items-center">{children}</main>
+          <BottomNavigation />
+        </div>
+      </UserProvider>
+    </>
+  );
+}
--- src/pages/[name]/index.tsx
+++ src/pages/[name]/index.tsx
@@ -1,8 +1,5 @@
-import Head from "next/head";
 import { useSession } from "next-auth/react";
 import Router from "next/router";
-import { UserProvider } from "../../lib/UserContext";
-import { BottomNavigation } from "../../components/navigation";
 import {
   ClipboardDocumentCheckIcon,
   Cog6ToothIcon,
@@ -12,6 +9,7 @@ import {
 } from "@heroicons/react/24/outline";
 import { StarIcon, UserGroupIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
+import ProfileLayout from "~/components/profileLayout";
 
 const userStats = [
   {
@@ -54,60 +52,56 @@ export default function Profile() {
   if (!sessionData || !sessionData?.user.name) return;
 
   return (
-    <>
-      <Head>
-        <title>{sessionData.user.name}</title>
-      </Head>
-      <UserProvider
-        id={sessionData.user.id}
-        name={sessionData.user.name || ""}
-        email={sessionData.user.email || ""}
-      >
-        <div className="flex h-screen flex-col">
-          <main className="mb-[86px] flex h-full flex-col items-center">
-            <div className="relative h-32 w-full bg-teal">
-              <Link
-                href={`/${sessionData.user.name}/configuracion`}
-                className="no-highlight absolute right-3 top-3 rounded-md bg-orange p-1"
-              >
-                <Cog6ToothIcon className="h-6 w-6 text-white" />
-              </Link>
-              <div className="absolute bottom-0 left-8 flex h-[120px] w-[120px] translate-y-1/2 items-center justify-center rounded-full border-[3px] border-teal bg-[#d9d9d9]">
-                <span className="text-7xl text-teal">
-                  {sessionData.user.name[0]?.toLocaleUpperCase()}
-                </span>
-              </div>
-              <span className="absolute bottom-1 left-1/2 -translate-x-2 text-2xl text-white">
-                {sessionData.user.name}
-              </span>
-            </div>
-            <div className="flex w-full items-center justify-center px-7 pt-24">
-              <div className="grid w-full grid-cols-2 gap-6 rounded-xl bg-[#d9d9d9] p-4">
-                {userStats.map((stat) => (
-                  <div
-                    key={stat.label}
-                    className="flex flex-col items-center text-center"
-                  >
-                    {stat.icon}
-                    <span className="text-lg font-medium text-orange">
-                      {stat.number}
-                    </span>
-                    <span className="-mt-2 text-lg font-medium">
-                      {stat.label.split(" ")[0]}
-                    </span>
-                    {stat.label.split(" ").length > 1 && (
-                      <span className="-mt-2 font-medium">
-                        {stat.label.split(" ")[1]}
-                      </span>
-                    )}
-                  </div>
-                ))}
-              </div>
-            </div>
-          </main>
-          <BottomNavigation />
+    <ProfileLayout
+      header={<Header userName={sessionData.user.name} />}
+      sessionData={sessionData}
+    >
+      <div className="relative h-[72px] w-full bg-teal">
+        <div className="absolute bottom-0 left-8 flex h-[120px] w-[120px] translate-y-1/2 items-center justify-center rounded-full border-[3px] border-teal bg-[#d9d9d9]">
+          <span className="text-7xl text-teal">
+            {sessionData.user.name[0]?.toLocaleUpperCase()}
+          </span>
         </div>
-      </UserProvider>
-    </>
+        <span className="absolute bottom-1 left-1/2 -translate-x-2 text-2xl text-white">
+          {sessionData.user.name}
+        </span>
+      </div>
+      <div className="flex w-full items-center justify-center px-7 pt-24">
+        <div className="grid w-full grid-cols-2 gap-6 rounded-xl bg-[#d9d9d9] p-4">
+          {userStats.map((stat) => (
+            <div
+              key={stat.label}
+              className="flex flex-col items-center text-center"
+            >
+              {stat.icon}
+              <span className="text-lg font-medium text-orange">
+                {stat.number}
+              </span>
+              <span className="-mt-2 text-lg font-medium">
+                {stat.label.split(" ")[0]}
+              </span>
+              {stat.label.split(" ").length > 1 && (
+                <span className="-mt-2 font-medium">
+                  {stat.label.split(" ")[1]}
+                </span>
+              )}
+            </div>
+          ))}
+        </div>
+      </div>
+    </ProfileLayout>
+  );
+}
+
+function Header({ userName }: { userName: string }) {
+  return (
+    <div className="flex w-full items-center justify-end bg-teal py-3 pr-3">
+      <Link
+        href={`/${userName}/configuracion`}
+        className="no-highlight rounded-md bg-orange p-1"
+      >
+        <Cog6ToothIcon className="h-6 w-6 text-white" />
+      </Link>
+    </div>
   );
 }
