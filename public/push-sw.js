42e3b1377e7cd359dda3018014397d05b760dfb0
IanPol0 <101563167+IanPol0@users.noreply.github.com>
2023-10-09 10:46:46 -0300

    Update push-sw.js

--- public/push-sw.js
+++ public/push-sw.js
@@ -1,6 +1,9 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 self.addEventListener('push', event => {
+  // @ts-ignore
   const data = event.data.json();
   console.log('New notification', data);
+  // @ts-ignore
   self.registration.showNotification(data.title, {
     body: data.body,
     icon: "./favicon.ico"
