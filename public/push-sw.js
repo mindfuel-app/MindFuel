b3c79d27db007165b9d9dac0a026c4a907b7f0e4
IanPol0 <ipolonuer@gmail.com>
2023-10-08 21:24:38 -0300

    Update push-sw.js

--- public/push-sw.js
+++ public/push-sw.js
@@ -1,9 +1,14 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 self.addEventListener('push', event => {
+  // @ts-ignore
   const data = JSON.parse(event.data)
   console.log('New notification', data);
 
+  // @ts-ignore
   self.registration.showNotification(data.title, {
     body: data.body,
     icon: "./favicon.ico"
     });
   });
+
+  
