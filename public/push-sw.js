b7818fe55619f6f18895d5b025cf11ec5c961f3e
IanPol0 <101563167+IanPol0@users.noreply.github.com>
2023-10-09 10:44:33 -0300

    Update push-sw.js

--- public/push-sw.js
+++ public/push-sw.js
@@ -2,7 +2,7 @@ self.addEventListener('push', event => {
   const data = event.data.json();
   console.log('New notification', data);
   self.registration.showNotification(data.title, {
-    body: data.message,
+    body: data.body,
     icon: "./favicon.ico"
     });
   });
