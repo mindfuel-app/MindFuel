c5cafad46f0fb479dd5d5cf94a6c754e49b7016b
IanPol0 <101563167+IanPol0@users.noreply.github.com>
2023-10-12 12:09:42 -0300

    feat: created notes router

---
+++ src/server/api/routers/notes.ts
@@ -0,0 +1,42 @@
+import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
+import { z } from "zod";
+import { error } from "console";
+
+export const notesRouter = createTRPCRouter({
+    getNotes: protectedProcedure
+    .input(z.object({ userId: z.string() }))
+    .query(async ({ ctx, input }) => {
+      const notes = await ctx.prisma.note.findMany({
+        where: {
+          user_id: input.userId,
+        },
+      });
+
+      return notes;
+    }),
+
+    createNote: protectedProcedure
+    .input(z.object({ userId: z.string(), note: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      const note = await ctx.prisma.note.create({
+        data: {
+          user_id: input.userId,
+          note: input.note,
+        },
+      });
+
+      return note;
+    }),
+
+    deleteNote: protectedProcedure
+    .input(z.object({ noteId: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      const note = await ctx.prisma.note.delete({
+        where: {
+          id: input.noteId,
+        },
+      });
+
+      return note;
+    }),
+});
