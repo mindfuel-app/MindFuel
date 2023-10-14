08d50ab62209cf013381fbfb7597b33e2c5f0b10
IanPol0 <ipolonuer@gmail.com>
2023-10-14 15:24:05 -0300

    Update cron.ts

--- src/pages/api/cron.ts
+++ src/pages/api/cron.ts
@@ -1,13 +1,14 @@
 import { NextResponse } from 'next/server';
 import { api } from '~/utils/api';
+import prisma from '~/utils/prisma';
  
 export const config = {
   runtime: 'edge',
 };
  
-export default function handler() {
+export default async function handler() {
   try {
-    const deletedValues = api.selfCare.clearValues.useMutation({});
+    const deletedValues = await prisma.selfCare.deleteMany({});
     return NextResponse.json({ deletedValues });
   } catch (error) {
     console.error(error);
