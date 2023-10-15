cd3d3c9bd5cf8293694ed6277a281c498d407cad
dangoldmann <goldmandan8@gmail.com>
2023-10-15 17:22:01 -0300

    Formatted code

--- src/server/api/routers/selfCare.ts
+++ src/server/api/routers/selfCare.ts
@@ -1,24 +1,27 @@
-import { createTRPCRouter, protectedProcedure, publicProcedure } from "~/server/api/trpc";
+import {
+  createTRPCRouter,
+  protectedProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
 import { z } from "zod";
 
 export const selfCareRouter = createTRPCRouter({
-  clearValues: publicProcedure
-    .input(z.object({}))
-    .mutation(async ({ ctx }) => {
-      const deleted = await ctx.prisma.selfCare.deleteMany({});
-      return { deleted };
-    }),
-  getValues: protectedProcedure
-  .input(z.object({
-    user_id: z.string(),
-  }))
-  .query(async ({ input, ctx }) => {
-    const values = await ctx.prisma.selfCare.findMany({
-      where: {
-        user_id: input.user_id,
-      },
-    });
-    return { values };
+  clearValues: publicProcedure.input(z.object({})).mutation(async ({ ctx }) => {
+    const deleted = await ctx.prisma.selfCare.deleteMany({});
+    return { deleted };
   }),
-  })
-
+  getValues: protectedProcedure
+    .input(
+      z.object({
+        user_id: z.string(),
+      })
+    )
+    .query(async ({ input, ctx }) => {
+      const values = await ctx.prisma.selfCare.findMany({
+        where: {
+          user_id: input.user_id,
+        },
+      });
+      return { values };
+    }),
+});
