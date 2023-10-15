82203afe12c2be81d8d58ad1d57eeae31e06980f
IanPol0 <ipolonuer@gmail.com>
2023-10-15 16:46:23 -0300

    updated prisma

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -1,4 +1,6 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-const prisma = new PrismaClient();
+
+const prisma = new PrismaClient().$extends(withAccelerate())
 export default prisma;
