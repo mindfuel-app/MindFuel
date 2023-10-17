4929fdb08f1ff095b70054e0a384984419bae858
SANDRI FRANCO AGUSTIN <46960579@ort>
2023-10-17 13:37:00 -0300

    fix: changed CORS origin

--- ia/chatbot.py
+++ ia/chatbot.py
@@ -15,7 +15,7 @@ openai.api_key = os.environ.get("OPENAI_API_KEY")
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://mindfuel-ia.onrender.com","http://localhost:3000"],
+    allow_origins=["https://mindfuel.vercel.app","http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["GET","POST","OPTIONS"],
     allow_headers=["*"]
