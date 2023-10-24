dec859a5438bc0fd1566cba6c93fa40940195e66
FrancoSandri <46960579@est.ort.edu.ar>
2023-10-24 10:41:17 -0300

    fix: mysql connection

--- ia/mindfuel.py
+++ ia/mindfuel.py
@@ -1,6 +1,7 @@
 from flask import Flask, request,jsonify
 import pandas as pd
 import re
+import csv
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
@@ -18,37 +19,30 @@ def home():
     return "hello world"
 
 @app.route("/recomendar", methods = ['GET','POST'])
-
+    
 def recomendar():
-
-    #conexión al .env
-    db_host = os.getenv("DB_HOST")
-    db_user = os.getenv("DB_USER")
-    db_password = os.getenv("DB_PASSWORD")
-    db_database = os.getenv("DB_DATABASE")
-
-    #conexion a la base de datos
-    conn = sql.connect(host=db_host,
-                   user=db_user,
-                   password=db_password,
-                   db=db_database,
-                    )
+    conn = sql.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USERNAME"),
+        passwd=os.getenv("DB_PASSWORD"),
+        db=os.getenv("DB_NAME")
+    )
     
     cursor = conn.cursor()
 
-    #query: falta sacar los datos necesarios para la recomendación
-    cursor.execute("SELECT * FROM task")
+    cursor.execute("SELECT name, description, estimated_time, id  FROM task")
     result = cursor.fetchall()
-    rows_list = []
-    for row in result:
-        rows_list.append(row)
     cursor.close()
 
-    for row in rows_list:
-        print(row)
+    # Guardar los resultados en un archivo CSV
+    with open('tareas.csv', 'w', newline='', encoding='utf-8') as csvfile:
+        fieldnames = ['Tareas', 'Descripcion', 'Duracion', 'Id']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for row in result:
+            writer.writerow({'Tareas': row[0], 'Descripcion': row[1], 'Duracion': row[2], 'Id': row[3]})
 
-    #borrar una vez hecha la conexion con la base de datos
-    tareas = pd.read_csv("./input/tareas.csv")
+    tareas = pd.read_csv('tareas.csv')
     ratings = pd.read_csv("./input/puntuacion.csv")
 
     def clean_title(title):
@@ -56,49 +50,59 @@ def recomendar():
 
     tareas["clean_title"] = tareas["Tareas"].apply(clean_title)
 
-    vectorizer = TfidfVectorizer(ngram_range=(1, 2))
+    print(tareas)
+    return "hello world"
+    # tareas = pd.read_csv('tareas.csv')
+    # ratings = pd.read_csv("./input/puntuacion.csv")
 
-    tfidf = vectorizer.fit_transform(tareas["clean_title"])
+    # def clean_title(title):
+    #     return re.sub("[^a-zA-Z0-9 ]", "", title)
 
-    def search(title):
-        title = clean_title(title)
-        query_vec = vectorizer.transform([title])
-        similarity = cosine_similarity(query_vec, tfidf).flatten()
-        indices = np.argpartition(similarity, -5)[-5:]
-        results = tareas.iloc[indices]
-        return results
+    # tareas["clean_title"] = tareas["Tareas"].apply(clean_title)
 
-    def find_similar_tarea(tarea_id):
-        similar_users = ratings[(ratings["tarea_id"] == tarea_id) & (ratings["rating"] > 4)]["user_id"].unique()
-        similar_user_recs = ratings[(ratings["user_id"].isin(similar_users)) & (ratings["rating"] > 4)]["tarea_id"]
-        similar_user_recs = similar_user_recs.value_counts() / len(similar_users)
-        similar_user_recs = similar_user_recs[similar_user_recs > .10]
-        all_users = ratings[(ratings["tarea_id"].isin(similar_user_recs.index)) & (ratings["rating"] > 4)]
-        all_user_recs = all_users["tarea_id"].value_counts() / len(all_users["user_id"].unique())
+    # vectorizer = TfidfVectorizer(ngram_range=(1, 2))
 
-        rec_percentages = pd.concat([similar_user_recs, all_user_recs], axis=1)
-        rec_percentages.columns = ["similar", "all"]
+    # tfidf = vectorizer.fit_transform(tareas["clean_title"])
 
-        rec_percentages["score"] = rec_percentages["similar"] / rec_percentages["all"]
+    # def search(title):
+    #     title = clean_title(title)
+    #     query_vec = vectorizer.transform([title])
+    #     similarity = cosine_similarity(query_vec, tfidf).flatten()
+    #     indices = np.argpartition(similarity, -5)[-5:]
+    #     results = tareas.iloc[indices]
+    #     return results
 
-        rec_percentages = rec_percentages.sort_values("score", ascending=False)
-        rec_percentages = rec_percentages.head(10).merge(tareas, left_index=True, right_on="Id")[["Tareas", "Area"]]
-        return rec_percentages
+    # def find_similar_tarea(tarea_id):
+    #     similar_users = ratings[(ratings["tarea_id"] == tarea_id) & (ratings["rating"] > 4)]["user_id"].unique()
+    #     similar_user_recs = ratings[(ratings["user_id"].isin(similar_users)) & (ratings["rating"] > 4)]["tarea_id"]
+    #     similar_user_recs = similar_user_recs.value_counts() / len(similar_users)
+    #     similar_user_recs = similar_user_recs[similar_user_recs > .10]
+    #     all_users = ratings[(ratings["tarea_id"].isin(similar_user_recs.index)) & (ratings["rating"] > 4)]
+    #     all_user_recs = all_users["tarea_id"].value_counts() / len(all_users["user_id"].unique())
 
-    # Mostrar la tabla de tareas
-    data = request.form
-    title = data.get('tareas', '')
-    print(title)
-    if len(title) > 5:
-        results = search(title)
-        tarea_id = results.iloc[0]["Id"]
-        recomendacion = find_similar_tarea(tarea_id)
-    else:
-        recomendacion = pd.DataFrame()
-        warning = "Ingrese por lo menos 5 caracteres."
-        return jsonify(warning)
-    recomendacion_data = recomendacion.to_dict(orient='records')
-    return jsonify(recomendacion_data)
+    #     rec_percentages = pd.concat([similar_user_recs, all_user_recs], axis=1)
+    #     rec_percentages.columns = ["similar", "all"]
+
+    #     rec_percentages["score"] = rec_percentages["similar"] / rec_percentages["all"]
+
+    #     rec_percentages = rec_percentages.sort_values("score", ascending=False)
+    #     rec_percentages = rec_percentages.head(10).merge(tareas, left_index=True, right_on="Id")[["Tareas", "Area"]]
+    #     return rec_percentages
+
+    # # Mostrar la tabla de tareas
+    # data = request.form
+    # title = data.get('tareas', '')
+    # print(title)
+    # if len(title) > 5:
+    #     results = search(title)
+    #     tarea_id = results.iloc[0]["Id"]
+    #     recomendacion = find_similar_tarea(tarea_id)
+    # else:
+    #     recomendacion = pd.DataFrame()
+    #     warning = "Ingrese por lo menos 5 caracteres."
+    #     return jsonify(warning)
+    # recomendacion_data = recomendacion.to_dict(orient='records')
+    # return jsonify(recomendacion_data)
     
 
 if __name__ == "__main__":
