31c32ec69cf3435e6d681b85b949f714fcab152c
IanPol0 <101563167+IanPol0@users.noreply.github.com>
2023-11-02 11:09:20 -0300

    commenting edge functions because vercel is down

--- src/pages/api/cron.ts
+++ src/pages/api/cron.ts
@@ -1,21 +1,21 @@
-import { NextResponse } from "next/server";
-import { PrismaClient } from "@prisma/client";
+// import { NextResponse } from "next/server";
+// import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
-export const config = {
-  runtime: "edge",
-};
+// const prisma = new PrismaClient();
+// export const config = {
+//   runtime: "edge",
+// };
 
-export default async function handler() {
-  try {
-    const deletedValues = await prisma.water.updateMany({
-      data: {
-        water: 0,
-      },
-    });
-    return NextResponse.json({ deletedValues });
-  } catch (error) {
-    console.error(error);
-    return NextResponse.json({ error: error }, { status: 500 });
-  }
-}
+// export default async function handler() {
+//   try {
+//     const deletedValues = await prisma.water.updateMany({
+//       data: {
+//         water: 0,
+//       },
+//     });
+//     return NextResponse.json({ deletedValues });
+//   } catch (error) {
+//     console.error(error);
+//     return NextResponse.json({ error: error }, { status: 500 });
+//   }
+// }
