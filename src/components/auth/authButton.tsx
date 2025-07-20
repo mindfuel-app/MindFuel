597cedfacf01653d9d09edac0e61031617d2ae64
IanPol0 <ipolonuer@gmail.com>
2025-07-20 17:20:57 -0300

    fix: correct import path for Button component in AuthButton

--- src/components/auth/authButton.tsx
+++ src/components/auth/authButton.tsx
@@ -1,4 +1,4 @@
-import { Button } from "../../../components/ui/button";
+import { Button } from "../ui/button";
 import { CircularProgress } from "@mui/material";
 
 export default function AuthButton({
